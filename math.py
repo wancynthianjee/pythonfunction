dc30d2da3a97ba50eb744e148f610dabcd86c0e9
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-03-31 22:17:16 +0300

    commit

---
+++ math.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    answer=a + b
+    return sum
+
+    def subtract(c,d):
+        answer= c-d
+        return answer
+
+    def multiply(e,f):
+        answer = e*f
+        return answer
+   
+
+    def divide(g,h):
+        answer=g / h
+        return answer
